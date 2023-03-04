877ed5ab05ed595b9ff088c539e39e197af602f3
Asad Ali <asadali27232@gmail.com>
2023-03-04 15:39:43 +0500

    Updated

--- App.js
+++ App.js
@@ -1,18 +1,15 @@
-import { StatusBar } from "expo-status-bar"
-import { StyleSheet, Text, View } from "react-native"
+import { StyleSheet, View } from "react-native"
 import ForgotPassword from "./Screens/ForgotPassword"
 import LandingPage from "./Screens/LandingPage"
 import Login from "./Screens/Login"
 import ResetPassword from "./Screens/ResetPassword"
 import SignUp from "./Screens/SignUp"
 import Verification from "./Screens/Verification"
-import FoodieButton from "./Components/FoodieButton"
-import FoodieInput from "./Components/FoodieInput"
 
 export default function App() {
     return (
         <View style={styles.container}>
-            <Verification />
+            <SignUp />
         </View>
     )
 }
