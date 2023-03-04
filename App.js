16a69cfffcf0038b860165f59b3c7bba8d1e7d70
Asad Ali <asadali27232@gmail.com>
2023-03-04 11:05:22 +0500

    Updated

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from "expo-status-bar"
 import { StyleSheet, Text, View } from "react-native"
+import ForgotPassword from "./Screens/ForgotPassword"
 
 export default function App() {
     return (
         <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
+            <ForgotPassword />
             <StatusBar style="auto" />
         </View>
     )
---
+++ Screens/ResetPassword.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { StyleSheet, View, Text } from "react-native-web"
+
+export default function ForgotPassword() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.font}>Reset Password</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    font: {
+        fontSize: 26,
+    },
+})
